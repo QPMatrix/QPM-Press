ce116f8f2b52989734166eed1154e36900c6c02d
QPM <admin@QuantumPinnacleMatrix.tech>
2024-03-13 19:32:11 +0200

    updating the next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    crossOrigin: 'anonymous',
+    reactStrictMode:false,
+    images:{
+        domains:[
+            'uploadthing.com',
+            'utfs.io',
+            'img.clerk.com',
+            'subdomain',
+            'files.stripe.com'
+        ]
+    }
+};
 
 export default nextConfig;
