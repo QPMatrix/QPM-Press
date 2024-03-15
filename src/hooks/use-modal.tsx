73449b6bbd90c611b431071ec63b47b807276a9a
QPM <admin@QuantumPinnacleMatrix.tech>
2024-03-16 00:51:37 +0200

    making on modal from scratch adding the hooks and provider

---
+++ src/hooks/use-modal.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ModalContext } from "@/providers/modal-provider";
+import { useContext } from "react";
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModal must be used within a modal provider");
+  }
+  return context;
+};
