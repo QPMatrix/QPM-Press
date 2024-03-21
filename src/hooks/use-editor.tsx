8e072fbf534a857397b01c1314a0feb8cdacc4c6
QPM <admin@QuantumPinnacleMatrix.tech>
2024-03-21 16:42:01 +0200

    Done with editor provider + actions + hook at this moment

---
+++ src/hooks/use-editor.tsx
@@ -0,0 +1,12 @@
+import { EditorContext } from '@/providers/editor/editor-provider';
+import { useContext } from 'react';
+
+export const useEditor = () => {
+  const context = useContext(EditorContext);
+  if (!context) {
+    throw new Error(
+      'the use Editor Hook must be used within the editor provider',
+    );
+  }
+  return context;
+};
