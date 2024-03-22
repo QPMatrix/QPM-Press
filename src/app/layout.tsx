cdbcb00d17dbde9d12cac7128ef2a04c0c12bfc1
QPM <admin@QuantumPinnacleMatrix.tech>
2024-03-23 01:16:37 +0200

    Adding favicon icons for androiend and apple

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { DM_Sans } from 'next/font/google';
+import './globals.css';
 
-import { ThemeProvider } from "@/providers/theme-provider";
-import ModalProvider from "@/providers/modal-provider";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as SonnarToaster } from "@/components/ui/sonner";
+import { ThemeProvider } from '@/providers/theme-provider';
+import ModalProvider from '@/providers/modal-provider';
+import { Toaster } from '@/components/ui/toaster';
+import { Toaster as SonnarToaster } from '@/components/ui/sonner';
 
-const font = DM_Sans({ subsets: ["latin"] });
+const font = DM_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "QubeFlow",
-  description: "All in one Agency Solution",
+  title: 'QubeFlow',
+  description: 'All in one Agency Solution',
 };
 
 export default function RootLayout({
@@ -21,6 +21,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={font.className}>
         <ThemeProvider
           attribute="class"
